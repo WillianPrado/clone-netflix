bc18e44de28e9a4ecc81a90140c49061a8e40032
willian <willian95prado@gmail.com>
2021-01-18 14:34:22 -0300

    commit 1

---
+++ js/owl/setup.js
@@ -0,0 +1,35 @@
+/*
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 6,
+    spaceBetween: 10,
+    slidesPerGroup: 2,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
+let movies = [
+    {title : "Inception", imageUrl: "add image url"},
+    {title : "Sons of Anarchy", imageUrl: "add image url"}
+  ];
+
+  function myFunction() {
+    let result = "";
+    movies.forEach(function (item) {
+      result += "<div class='swiper-slide'><img src='"+ item.imageUrl +"' alt='alt text'> " + item.title + " ";
+    });
+    
+    document.getElementById("listMovies").innerHTML = result;
+  }
+  
+  myFunction();
+
+  <div id="listMovies" class="swiper-wrapper">
+</div>
+*/
